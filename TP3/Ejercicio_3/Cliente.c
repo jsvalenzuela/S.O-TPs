9f070c4cc2dcaad8c63537572bcc683d2eaf3475
jsvalenzuela <santi_1195@hotmail.com>
2016-06-20 15:05:58 -0300

    actualizado cliente

--- TP3/Ejercicio_3/Cliente.c
+++ TP3/Ejercicio_3/Cliente.c
@@ -8,14 +8,17 @@
 #define ESTRUCTURA_FILE "archivo"
 
 int getrusage(int who, struct rusage *usage);
+long get_micro();
+long get_micro_desde_timeval(const struct timeval tiempo);
 
 int main(void)
 {
+ long tiempo_arranque = get_micro();
  struct rusage usage;
- long tiempoSistema;
+ long tiempo_sistema,tiempo_total,tiempo_usuario;
  int i=0;
  FILE *fp;
-
+ 
  char readbuf[1000]="acávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kbysevaarepetirestetextohastaquellegueacávamicadenarelocaquierotenerunlargode1kby";
 ;
  /* Crea el FIFO si no existe */
@@ -37,11 +40,26 @@ int main(void)
  }
 
  getrusage(RUSAGE_SELF,&usage);
- tiempoSistema = usage.ru_stime.tv_sec * 1e6 + usage.ru_stime.tv_usec;
- printf ("Tiempo de CPU del proceso A: %ld.%06ld sec user, %ld.%06ld sec system bloques entrada %ld bloques salida%ld\n",
-          usage.ru_utime.tv_sec, usage.ru_utime.tv_usec,
-          usage.ru_stime.tv_sec, usage.ru_stime.tv_usec, usage.ru_inblock, usage.ru_oublock);
+ long tiempo_total = get_micro() - tiempo_arranque;
+ long tiempo_sistema = get_micro_desde_timeval(usage.ru_stime);
+ long tiempo_usuario = get_micro_desde_timeval(usage.ru_utime);
+ 
+ printf ("Tiempo de CPU del proceso A: %ld.%06ld sec reloj, %ld.%06ld sec user, %ld.%06ld sec system bloques entrada %ld bloques salida%ld\n",
+          tiempo_total,tiempo_total,
+          tiempo_usuario, tiempo_usuario,
+          tiempo_sistema, tiempo_sistema, usage.ru_inblock, usage.ru_oublock);
 
 
  return(0);
+}
+ 
+ 
+ long get_micro() {
+  struct timespec tiempo_inicial;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &tiempo_inicial);
+  return tiempo_inicial.tv_sec * 1e6 + tiempo_inicial.tv_nsec / 1e3;
+}
+
+long get_micro_desde_timeval(const struct timeval tiempo) {
+  return tiempo.tv_sec * 1e6 + tiempo.tv_usec;
 }
